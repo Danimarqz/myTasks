f24544033e00de3517b5673452a8f340d63bc9b9
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-04 23:34:19 +0200

    react+vite started

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hola Mykyta, me voy ir a dormir</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Has clicado en este botón {count} veces
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the React logo to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
