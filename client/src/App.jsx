d6e2eb007dbcb50917bcb51a03447d1ed7fc3879
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-08 17:56:20 +0200

    logout working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { TaskLogin } from "./pages/TaskLogin"
 import { TaskRegister } from "./pages/TaskRegister";
 import {Navigation} from "./components/Navigation";
 import {Toaster} from "react-hot-toast";
+import { TaskLogout } from "./pages/TaskLogout";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     <Navigation />
       <Routes>
       <Route path="/login" element={<TaskLogin/>} />
-      <Route path="/logout" element={<TaskLogin/>} />
+      <Route path="/logout" element={<TaskLogout/>} />
       <Route path="/register" element={<TaskRegister/>} />
       <Route path="/tasks" element={<TasksPage />} />
       <Route path="/tasks-create" element={<TasksFormPage />} />
---
+++ client/src/pages/TaskLogout.jsx
@@ -0,0 +1,6 @@
+export function TaskLogout(){
+    localStorage.clear();
+    return (
+            <p className='text-white font-bold text-xl'>See you soon!</p>
+    )
+}
